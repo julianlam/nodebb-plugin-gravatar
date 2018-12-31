julianlam/nodebb-plugin-gravatar

commit af1c206eda1b0e78c2c164b45af7bfe67a2c6ba2

Update library.js
Fix path

library.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var user = module.parent.require('./user'),
-	meta = module.parent.require('./meta'),
-	db = module.parent.require('./database'),
+var user = module.parent.require('./src/user'),
+	meta = module.parent.require('./src/meta'),
+	db = module.parent.require('./src/database'),
 	winston = module.parent.require('winston'),
 	async = module.parent.require('async'),
 	crypto = require('crypto'),

commit e90f8bd8ddbf3b8b621889aafe4411a0ff89ad52

fix: broken requires introduced by #18

library.js
@@ -1,10 +1,10 @@
 "use strict";
 
-var user = module.parent.require('./src/user'),
-	meta = module.parent.require('./src/meta'),
-	db = module.parent.require('./src/database'),
-	winston = module.parent.require('winston'),
-	async = module.parent.require('async'),
+var user = require.main.require('./src/user'),
+	meta = require.main.require('./src/meta'),
+	db = require.main.require('./src/database'),
+	winston = require.main.require('winston'),
+	async = require('async'),
 	crypto = require('crypto'),
 
 	controllers = require('./lib/controllers'),